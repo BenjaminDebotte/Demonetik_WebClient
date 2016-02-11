d4b740b63e54f9f069ae997775fa7bb0589a85d5
Benjamin Debotté <benjamin.debotte@ecole.ensicaen.fr>
2016-02-11 17:34:02 +0100

    Mise en place de la websocket Affichage des résultats dans la page Gestion de la dynamique de la barre de chargement

--- WebContent/API.js
+++ WebContent/API.js
@@ -1,20 +1,42 @@
+function indexToWidth(index) {
+		//console.log(index * 4 + '%');
+	  return index + '%';
+}
+
+
 var socket = new WebSocket("ws://192.168.43.233:8080/DemonetikWebService/websocketdemonetik");
 
 socket.onmessage = function(event){
-	  var msg = JSON.parse(event.data);
 	  
+	
+	var msg = JSON.parse(event.data);
+	console.log(msg);  
 	  
+	if(msg.numEtat == "1") {
+		$('.carte_list').text("");
+		$('.tpe_list').text("");
+		$('.banque_list').text("");
+	}
+	
+	
+	
+	
 	  switch(msg.type) {
 	    case "Carte":
-	  	  $('.carte_list').append("<li>" + msg.text + "</li>\n");
+	  	  $('.carte_list').append("<li>" + msg.libelle + "</li>\n");
 	      break;
 	    case "TPE":
-	  	  $('.tpe_list').append("<li>" + msg.text + "</li>\n");
+	  	  $('.tpe_list').append("<li>" + msg.libelle + "</li>\n");
 	      break;
 	    case "Banque":
-	  	  $('.banque_list').append("<li>" + msg.text + "</li>\n");
+	  	  $('.banque_list').append("<li>" + msg.libelle + "</li>\n");
 	      break;
 	  }
+	  
+	  
+	  //Mise à jour de la barre de chargement
+  		$('#BlueBar').css("width", indexToWidth(parseInt(msg.numEtat) * 20));
+
 	 
 };
 
@@ -22,3 +44,11 @@ socket.onopen = function(event) {
 	console.log("Connecté à la socket");
 	//socket.send("Bonjour !");
 };
+
+socket.onclose = function(event) {
+	console.log("Fermeture de la socket.");
+}
+
+
+
+
